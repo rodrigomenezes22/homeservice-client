d05da86998881c6a487b8b48797a7f099a21b842
Priyanka <priyankadavara@gmail.com>
2023-03-27 15:21:10 +0200

    added category image upload form

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ImageUploadForm from "./PropertyImage";
-
+import CategoryUploadForm from "./Category";
 function Home() {
   return (
     <div>
       Home Page
       <ImageUploadForm />
+      <CategoryUploadForm />
     </div>
   );
 }
